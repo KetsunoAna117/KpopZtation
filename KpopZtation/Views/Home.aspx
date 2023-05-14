49b01f90941acaead44c12c9dd2d8f86f3716a22
Hans Arthur <hans.cupiterson@binus.ac.id>
2023-05-14 13:17:52 +0700

    Added Navbar MasterPage

---
+++ KpopZtation/Views/Home.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Navbar.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="KpopZtation.Views.Home" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Home</h2>
+</asp:Content>
---
+++ KpopZtation/Views/Navbar.Master.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+
+namespace KpopZtation.Views
+{
+    public partial class Navbar : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
