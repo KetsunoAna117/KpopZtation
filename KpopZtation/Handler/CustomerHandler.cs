2a1ab8e52c606663880ce95678af799b0087cf58
Hans Arthur <hans.cupiterson@binus.ac.id>
2023-05-30 12:33:04 +0700

    Fix Handler so it apply the concept of PSD

--- KpopZtation/Handler/CustomerHandler.cs
+++ KpopZtation/Handler/CustomerHandler.cs
@@ -9,24 +9,21 @@ namespace KpopZtation.Handler
 {
     public class CustomerHandler
     {
-        
+
         public static Customer ValidateEmail(String email)
         {
-            List<Customer> customers = CustomerRepository.getAllCustomer();
-            return customers.Find(customer => customer.CustomerEmail == email);
+            return CustomerRepository.findCustomerByEmail(email);
         }
 
         public static Customer ValidateLogin(String email, String password)
         {
-            List<Customer> customers = CustomerRepository.getAllCustomer();
-            return customers.Find(customer => customer.CustomerEmail == email && customer.CustomerPassword == password);
+            return CustomerRepository.GetCustomer(email, password);
 
         }
 
         public static Customer getCustomer(int id)
         {
-            List<Customer> customers = CustomerRepository.getAllCustomer();
-            return customers.Find(customer => customer.CustomerID == id);
+            return CustomerRepository.getCustomerByID(id);
         }
 
         //public static bool isAdmin()
